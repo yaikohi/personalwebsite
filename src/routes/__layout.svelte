f17bc892f4d26a22a7fd4933749a063b87a9baf5
Erik Beem <erik.beem@hotmail.com>
2022-06-13 18:37:22 +0200

    Add stable background div

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,7 +7,9 @@
 </script>
 
 <div class:dark class="bg-white dark:bg-black">
-	<div class="mx-auto h-full bg-white dark:bg-black dark:text-white lg:px-2">
+	<div class="absolute inset-0 z-0 h-screen w-screen bg-white dark:bg-black" />
+
+	<div class="relative z-20 mx-auto h-full bg-white dark:bg-black dark:text-white lg:px-2">
 		<Navbar bind:dark />
 		<main>
 			<slot />
