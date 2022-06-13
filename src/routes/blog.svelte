b68b4f852b759930fd05f4599346983a629edc91
Erik Beem <erik.beem@hotmail.com>
2022-06-13 20:17:03 +0200

    WIP Add dynamic mock images

--- src/routes/blog.svelte
+++ src/routes/blog.svelte
@@ -2,19 +2,24 @@
 	/** @type {import('@sveltejs/kit').Load */
 	export const load: import('@sveltejs/kit').Load = async ({ fetch }) => {
 		const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-
 		const blogposts = await res.json();
+
+		const resImg = await fetch('https://dog.ceo/api/breed/leonberg/images/');
+		const resImgdata = await resImg.json();
+		const images = resImgdata.message;
 
 		return {
 			props: {
-				blogposts
+				blogposts,
+				images
 			}
 		};
 	};
 </script>
 
 <script lang="ts">
-	export let blogposts: unknown | any;
+	export let blogposts: unknown[] | any[];
+	export let images: unknown[] | any[];
 </script>
 
 <div class="container mx-auto px-6 md:max-w-xl lg:max-w-4xl ">
@@ -22,13 +27,12 @@
 	<h2>Contains posts and stuff</h2>
 	{#each blogposts as post}
 		<section class="py-4 flex flex-col">
-			<h2>{post.title.substring(0, 20)}</h2>
-			<p>{post.body.substring(0, 80)}</p>
-			<a href={`/blog/${post.id}`}>Read more...</a>
+			<img src={images[post.id]} alt="Leonberger dog" />
+			<h2 class="py-2">{post.title.substring(0, 20)}</h2>
+			<div class="pl-2">
+				<p>{post.body}</p>
+				<a href={`/blog/${post.id}`}>Read more...</a>
+			</div>
 		</section>
 	{/each}
-	<article>
-		<h2>Title post 1</h2>
-		<p>content post 1</p>
-	</article>
 </div>
--- src/routes/blog/[id].svelte
+++ src/routes/blog/[id].svelte
@@ -5,9 +5,18 @@
 		const id = params.id;
 		const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
 		const post = await res.json();
+
+		const imgRes = await fetch('https://dog.ceo/api/breed/leonberg/images/');
+		const imgJson: any = imgRes.json();
+
+		const images = imgJson.message;
+
+		const image = images[id];
+
 		return {
 			props: {
-				post
+				post,
+				image
 			}
 		};
 	};
@@ -15,10 +24,12 @@
 
 <script lang="ts">
 	export let post: { title: string; body: string };
+	export let image: string;
 </script>
 
 <div class="container mx-auto px-6 md:max-w-xl lg:max-w-4xl ">
 	<article class="py-4 flex flex-col">
+		<img src={image} alt="Leonberger dog" />
 		<h2 class="py-5">{post.title}</h2>
 		<p class="">{post.body}</p>
 	</article>
