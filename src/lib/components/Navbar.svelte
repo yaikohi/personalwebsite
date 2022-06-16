f30fdf20a3a26c74d1debe76f881b97e716a706a
Erik Beem <erik.beem@hotmail.com>
2022-06-16 16:46:49 +0200

    Add icons, remove footer

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import NavbarLink from './NavbarLink.svelte'
+	import HomeIcon from './icons/HomeIcon.svelte'
 	export let dark: boolean
 </script>
 
@@ -7,7 +8,7 @@
 	class="sticky top-0 z-20 mx-auto  bg-white bg-opacity-80 py-6 dark:bg-slate-900 dark:bg-opacity-80 sm:px-6 "
 >
 	<ul class="container mx-auto flex max-w-4xl items-center justify-between px-6">
-		<NavbarLink linksTo="/">Erik Beem</NavbarLink>
+		<NavbarLink linksTo="/"><HomeIcon /></NavbarLink>
 		<NavbarLink linksTo="/blog">Blog</NavbarLink>
 		<button
 			class="text-black dark:text-white md:text-xl"
