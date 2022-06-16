b64932e4f19db0bc11486abc2d867587d23cf035
Erik Beem <erik.beem@hotmail.com>
2022-06-16 12:01:44 +0200

    Add date-posted styling

--- src/routes/blog/[id].svelte
+++ src/routes/blog/[id].svelte
@@ -22,50 +22,67 @@
 
 	export let image: string
 	export let blogPost: TBlogPost
+
+	const longSubtitle =
+		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere in quos natus consequatur repudiandae tenetur ad eum accusamus est quisquam. Fugiat nihil explicabo odit suscipit harum optio libero consectetur rerum.'
+	const shortSubtitle = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+	const subtitle =
+		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere in quos natus consequatur repudiandae tenetur ad eum accusamus est quisquam.'
+
+	const today = new Date()
+	const date = new Intl.DateTimeFormat('en-GB', { dateStyle: 'full' }).format(today)
 </script>
 
-<div class="mx-auto block md:max-w-7xl">
-	<div class="px-6 md:max-w-xl lg:max-w-4xl flex flex-row justify-between ">
-		<div class="w-[80px] h-full">something</div>
-		<article class="mt-20 flex h-full flex-col py-4">
-			<h1 class="py-5 font-ptsans font-bold capitalize">{blogPost.title}</h1>
-
-			<div class="z-0 bg-slate-400 bg-gradient-to-l from-black to-transparent">
+<div class="mx-auto block max-w-7xl mt-20">
+	<div class="px-16 md:max-w-xl lg:max-w-4xl flex flex-row justify-between ">
+		<!-- <div class="h-full lg:block hidden bg-red-900">something</div> -->
+		<article class="flex h-full flex-col py-4">
+			<!-- Title -->
+			<h1 class="pt-5 font-ptsans font-bold capitalize">{blogPost.title}</h1>
+			<!-- Date posted -->
+			<p class="py-2 italic text-sm">
+				Date posted: <time datetime={date}> {date} </time>
+			</p>
+			<!-- Image -->
+			<div
+				class="z-0 bg-transparent mt-12 rounded-l-3xl bg-gradient-to-r from-slate-800 to-transparent"
+			>
 				<img
-					class="z-20 max-w-xs bg-white saturate-0 transition-all delay-200 ease-in-out hover:saturate-50 md:max-w-md "
+					class="z-20 pl-5 py-5 rounded-3xl saturate-100 transition-all delay-200 ease-in-out hover:saturate-200 "
 					src={image}
 					alt="dog"
 				/>
 			</div>
-			<div class="w-full max-h-44 border-l-4 border-l-slate-100 py-8 divide mb-8 mt-6">
+			<!--  Subtitle -->
+			<div class="w-full border-l-4 border-l-slate-100 py-8 divide mb-8 mt-6">
 				<h2 class="italic text-slate-400 pl-8 ">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere in quos natus consequatur
-					repudiandae tenetur ad eum accusamus est quisquam. Fugiat nihil explicabo odit suscipit
-					harum optio libero consectetur rerum.
+					{blogPost.title}
+					{blogPost.title}
 				</h2>
 			</div>
-			<p class="max-w-xl font-sourcesans text-xl">
+			<!-- Content, Paragraphs -->
+			<p class="max-w-3xl font-sourcesans text-xl">
+				Elle {blogPost.body}
 				{blogPost.body}
-				{blogPost.body}
+				{blogPost.body}.
 				<br />
 				<br />
-				{blogPost.body}
-				{blogPost.body}
+				Ire {blogPost.body}
+				{blogPost.body}.
 				<br />
 				<br />
+				Fuga {blogPost.body}
 				{blogPost.body}
-				{blogPost.body}
-				{blogPost.body}
+				{blogPost.body}.
 				<br />
 				{blogPost.body}
-				{blogPost.body}
+				{blogPost.body}.
 				<br />
 				<br />
+				A{blogPost.body}
 				{blogPost.body}
-				{blogPost.body}
-				{blogPost.body}
+				{blogPost.body}.
 			</p>
 		</article>
-		<div class="w-[400px] h-full">something</div>
 	</div>
 </div>
