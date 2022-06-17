0dd35e0dbd467eb0e5f0fea2761fa505b5e10c5e
yaikohi <erik.beem@hotmail.com>
2022-06-17 18:25:14 +0200

    Update layout w images. Seperate image from cards

--- src/routes/blog.svelte
+++ src/routes/blog.svelte
@@ -24,32 +24,50 @@
 	export let images: string[]
 </script>
 
-<div class="container mx-auto md:max-w-xl md:grid-cols-1 lg:max-w-6xl">
+<div class="container mx-auto md:max-w-xl lg:max-w-6xl max-w-xl ">
 	<section class="p-12 text-left">
 		<h1 class="pt-2 font-ptsans">Welcome to my &nbsp;'Work in progress' page</h1>
-		<h2 class="mt-7 py-2 font-ptsans text-4xl sm:mt-2">
-			Enjoy lorem ipsum and leonberger dogs here!
-		</h2>
+		<h2 class="mt-7 py-2 font-ptsans text-4xl sm:mt-2">Enjoy lorem ipsum and leonberger dogs!</h2>
 	</section>
-	<div class="grid mt-12 lg:grid-cols-2 ">
-		{#each posts as post}
-			<section
-				class="relative m-2 flex max-h-[600px] max-w-lg flex-col rounded-xl bg-slate-100 py-10 px-8 dark:bg-slate-800  dark:text-white"
-			>
+	<div class="flex flex-row">
+		<div class="flex flex-col">
+			{#each posts as post}
+				<section
+					class="relative m-8 flex max-h-[600px] max-w-lg flex-col rounded-xl bg-slate-100 px-8 dark:bg-slate-800  dark:text-white"
+				>
+					<Image
+						className="mb-4 overflow-hidden hidden rounded-xl object-scale-down md:object-fit saturate-100 transition-all delay-200 ease-in-out hover:saturate-200"
+						src={images[post.id]}
+						alt="Leonberger dog"
+					/>
+					<h2 class="pt-8 pb-4 font-ptsans font-bold capitalize">{post.title}</h2>
+					<div class="max-w-md overflow-x-scroll h-64">
+						<p class="font-sourcesans indent-4 font-light">
+							Qe
+							{post.body}
+							{post.body}
+							{post.body}
+							{post.body}
+						</p>
+					</div>
+					<a
+						class="pt-2 pb-8 font-sourcesans font-thin hover:font-medium "
+						href={`/blog/${post.id}`}>Read more...</a
+					>
+				</section>
+			{/each}
+		</div>
+		<div class="m-8 hidden md:flex gap-2 flex-col items-start">
+			<h2 class="mt-7 mb-12 italic font-thin py-2 font-ptsans text-2xl sm:mt-2">
+				I'll put Leonberger dogs here until I know what to do with this space.
+			</h2>
+			{#each posts as post}
 				<Image
-					className="mb-4 overflow-hidden rounded-xl object-cover saturate-100 transition-all delay-200 ease-in-out hover:saturate-200"
+					className="mb-4 rounded-xl object-scale-down md:object-fit saturate-100 transition-all delay-200 ease-in-out hover:saturate-200"
 					src={images[post.id]}
 					alt="Leonberger dog"
 				/>
-				<h2 class="py-2 font-ptsans font-bold capitalize">{post.title}</h2>
-				<div class="pl-2">
-					<p class="overflow-hidden py-2 font-sourcesans font-light">
-						{post.body}
-						{post.body}
-					</p>
-				</div>
-				<a class="py-4 font-sourcesans font-medium " href={`/blog/${post.id}`}>Read more...</a>
-			</section>
-		{/each}
+			{/each}
+		</div>
 	</div>
 </div>
