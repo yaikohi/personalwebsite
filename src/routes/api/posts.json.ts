7dfd01877697145263e1fa346aa650a943809ed5
yaikohi <erik.beem@hotmail.com>
2022-06-19 20:27:20 +0200

    Fix sort function for api posts

--- src/routes/api/posts.json.ts
+++ src/routes/api/posts.json.ts
@@ -1,32 +1,47 @@
+
+type TPost = {
+    title: string,
+    date: string,
+}
+
+type TPostFile = {
+    path: string,
+    meta: TPost
+}
+
 export const get = async () => {
-	/**
+    /**
 	 * vite function, imports files and returns an object
 	 * with the relative path as a key and the value being
 	 * a function that loads the contents as a Js Promise
 	 */
-	const allPostFiles = import.meta.glob('../blog/*.md')
+    const allPostFiles = import.meta.glob('../blog/*.md')
 
-	console.log(allPostFiles)
+    const iterablePostFiles = Object.entries(allPostFiles)
+  
+    const allPosts = await Promise.all(
+      iterablePostFiles.map(async ([path, resolver]) => {
+        const { metadata } = await resolver()
+        const postPath = path.slice(2, -3)
+  
+        return {
+          meta: metadata,
+          path: postPath,
+        }
+      })
+    )  
 
-	const iterablePostFiles = Object.entries(allPostFiles)
+    const sortedPosts = allPosts.sort((a: TPostFile, b: TPostFile) => {
+        const dateB = new Date(b.meta.date)
+        const dateA = new Date(a.meta.date)
 
-	const allPosts = await Promise.all(
-		iterablePostFiles.map(async ([path, resolver]) => {
-			const { metaData } = await resolver()
-			const postPath = path.slice(2, -3)
+        // src1: https://joshcollinsworth.com/blog/build-static-sveltekit-markdown-blog
+        // src2: https://bobbyhadz.com/blog/typescript-left-hand-side-of-arithmetic-operation-must-be-type
 
-			return {
-				meta: metaData,
-				path: postPath,
-			}
-		}),
-	)
-	// const sortedPosts = allPosts.sort((a, b) => {
-	//     return new Date(b.meta.date) - new Date(a.meta.date)
-	// })
-
-	return {
-		status: 200,
-		body: allPosts,
-	}
-}
+      return dateB.getTime() - dateA.getTime()
+    })
+  
+    return {
+      body: sortedPosts
+    }
+  }
--- src/routes/blog/_post.svelte
+++ src/routes/blog/_post.svelte
@@ -1,16 +1,19 @@
-<script lang="ts">
+<script>
+	// @ts-nocheck
+
 	/**
+	 * It is not yet possible to have typescript with mdsvex as of 2022-06-19.
+	 *
+	 *
 	 * This file starts with an underscore, meaning that this file
 	 * is private from the routing and will serve as a layout
 	 * file only.
-	 */
-
-	/**
+	 *
+	 *
 	 * Required for reading the 'frontmatter' of the markdown files
 	 */
-
-	export let title: string
-	export let date: string
+	export let title
+	export let date
 </script>
 
 <h1>{title}</h1>
