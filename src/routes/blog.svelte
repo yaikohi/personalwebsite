00b9f8cec33fa7f01b0aecaae2e1ac377a0e53b8
Erik Beem <erik.beem@hotmail.com>
2022-06-29 12:16:35 +0200

    update styling for blog & posts

--- src/routes/blog.svelte
+++ src/routes/blog.svelte
@@ -13,46 +13,43 @@
 </script>
 
 <script lang="ts">
-	import Post from './blog/_post.svelte'
-
 	export let posts: any
 </script>
 
 <div class="mx-auto max-w-xl">
-	<div class="container mx-auto max-w-xl md:max-w-xl lg:max-w-6xl ">
-		<div class="mx-8 ">
-			<section class="py-12 text-left">
-				<h1 class="pt-2 font-ptsans">Welcome to my &nbsp;'Work in progress' page</h1>
-				<h2 class="mt-7 py-2 font-ptsans text-4xl sm:mt-2">
-					Enjoy lorem ipsum and leonberger dogs!
-				</h2>
+	<div class=" mx-8 sm:mx-auto">
+		<div class="container max-w-xl md:max-w-xl lg:max-w-6xl ">
+			<section class="mt-14 mb-4 text-left">
+				<h1 class="font-ptsans">i am a blogger</h1>
+				<h2 class="font-ptsans text-4xl sm:mt-4">professional hobbyist and time waster</h2>
 			</section>
 			<div class="flex flex-row">
 				<div class="flex flex-col" />
-				<h2 class="pt-0 pb-8 font-ptsans text-2xl font-thin italic sm:mt-2">
-					Some text I can put here as some sort of side-note or something.
+				<h2 class="mt-0 mb-8 font-ptsans text-2xl font-thin italic sm:mt-2">
+					text-reader, video-watcher, youtube-subscriber, game-gamer, household-developer, and a
+					full-time person
 				</h2>
 			</div>
 		</div>
-	</div>
 
-	<section class="mx-auto max-w-xl">
-		<h2>Blogposts</h2>
-		<div class="flex flex-col p-4">
-			<ul>
-				{#each posts as post}
-					<li class="py-2">
-						<article>
-							<header>
-								<h3 class="font-medium py-2">
-									<a href={`${post.path}`}>{post.meta.title}</a>
-								</h3>
-								<p class="font-light">{post.meta.summary}</p>
-							</header>
-						</article>
-					</li>
-				{/each}
-			</ul>
-		</div>
-	</section>
+		<section class="mx-auto max-w-xl">
+			<h2>Blogposts</h2>
+			<div class="flex flex-col">
+				<ul>
+					{#each posts as post}
+						<li class="mt-4">
+							<article>
+								<header>
+									<h3 class="font-medium">
+										<a href={`${post.path}`}>{post.meta.title}</a>
+									</h3>
+									<p class="font-light">{post.meta.summary}</p>
+								</header>
+							</article>
+						</li>
+					{/each}
+				</ul>
+			</div>
+		</section>
+	</div>
 </div>
--- src/routes/blog/_post.svelte
+++ src/routes/blog/_post.svelte
@@ -17,7 +17,7 @@
 	export let summary
 </script>
 
-<article class="mx-auto max-w-2xl">
+<article class="sm:mx-auto max-w-2xl mx-8">
 	<header class="mx-auto max-w-2xl mt-20 mb-14">
 		<h1 class="pb-4 font-medium">{title}</h1>
 		<h2 class="italic font-light">{summary}</h2>
