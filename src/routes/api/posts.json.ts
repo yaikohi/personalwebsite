be6b713c621bde9f27354cd7dc9c31f8212f4c9b
yaikohi <erik.beem@hotmail.com>
2022-07-17 16:57:48 +0200

    Refactor types in endpoint / markdown loader, remove dead files

--- src/routes/api/posts.json.ts
+++ src/routes/api/posts.json.ts
@@ -1,24 +1,42 @@
-export type TPost = {
+/**
+ * Resources used:
+ * src1: https://joshcollinsworth.com/blog/build-static-sveltekit-markdown-blog
+ * src2: https://bobbyhadz.com/blog/typescript-left-hand-side-of-arithmetic-operation-must-be-type
+ */
+
+/**
+ * The frontmatter of the `.md` posts
+ */
+type Frontmatter = {
 	title: string
 	date: string
+	summary: string
 }
 
-export type TPostFile = {
+/**
+ * The function that returns a promise which resolves into the frontmatter `Post`.
+ */
+type Meta = () => Promise<{ metadata: Frontmatter }>
+
+/**
+ * Contains the metadata and the file path.
+ */
+type PostFile = {
 	path: string
-	meta: TPost
+	meta: Frontmatter
 }
 
-export const get = async () => {
+export const GET: import('@sveltejs/kit').RequestHandler = async () => {
 	/**
-	 * vite function, imports files and returns an object
-	 * with the relative path as a key and the value being
-	 * a function that loads the contents as a Js Promise
+	 * Sveltekit (or Vite(?)) function that imports files and returns an object
+	 * with the relative path as a key and the value as a
+	 *  function that loads the content as a `Promise`
 	 */
-	const allPostFiles = import.meta.glob('../blog/*.md')
+	const postFiles = import.meta.glob('../blog/*.md') as unknown as Record<string, Meta>
 
-	const iterablePostFiles = Object.entries(allPostFiles)
+	const iterablePostFiles = Object.entries(postFiles)
 
-	const allPosts = await Promise.all(
+	const posts = await Promise.all(
 		iterablePostFiles.map(async ([path, resolver]) => {
 			const { metadata } = await resolver()
 			const postPath = path.slice(2, -3)
@@ -30,12 +48,9 @@ export const get = async () => {
 		}),
 	)
 
-	const sortedPosts = allPosts.sort((a: TPostFile, b: TPostFile) => {
+	const sortedPosts = posts.sort((a: PostFile, b: PostFile) => {
 		const dateB = new Date(b.meta.date)
 		const dateA = new Date(a.meta.date)
-
-		// src1: https://joshcollinsworth.com/blog/build-static-sveltekit-markdown-blog
-		// src2: https://bobbyhadz.com/blog/typescript-left-hand-side-of-arithmetic-operation-must-be-type
 
 		return dateB.getTime() - dateA.getTime()
 	})
